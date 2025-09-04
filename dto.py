ec2106fd8df208cd79658a37f92cb4e6df726063
CDevv <cdevyt@gmail.com>
2025-09-04 16:41:48 +0300

    Use Many-to-Many relationships in the database

--- dto.py
+++ dto.py
@@ -1,13 +1,27 @@
+from typing import List
 from pydantic import BaseModel
 import datetime
+
+class ProjectTagResponse(BaseModel):
+    id: int
+    name: str
 
 class ProjectResponse(BaseModel):
     id: int
     name: str
     description: str
+    github: str | None
+    itch: str | None
+    weblink: str | None
+    tags: List[ProjectTagResponse]
+
+class BlogTagResponse(BaseModel):
+    id: int
+    name: str
 
 class BlogResponse(BaseModel):
     id: int
     name: str
     content: str
     date: datetime.date
+    tags: List[BlogTagResponse]
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import datetime
-from sqlalchemy import ForeignKey
+from typing import List
+from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy import String, Date
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
@@ -9,15 +10,56 @@ from sqlalchemy.orm import relationship
 class Base(DeclarativeBase):
     pass
 
+association_projects_tags = Table(
+    "project_tags_link",
+    Base.metadata,
+    Column("project_id", ForeignKey("projects.id"), primary_key=True),
+    Column("tag_id", ForeignKey("project_tags.id"), primary_key=True)
+)
+
+association_blogs_tags = Table(
+    "blog_tags_link",
+    Base.metadata,
+    Column("blog_id", ForeignKey("blogs.id"), primary_key=True),
+    Column("tag_id", ForeignKey("blog_tags.id"), primary_key=True)
+)
+
+class ProjectTag(Base):
+    __tablename__ = "project_tags"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+
+    projects: Mapped[List["Project"]] = relationship(
+        secondary=association_projects_tags, back_populates="tags"
+    )
+
 class Project(Base):
     __tablename__ = "projects"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String())
     description: Mapped[str] = mapped_column(String())
+    github: Mapped[str] = mapped_column(String(), nullable=True)
+    itch: Mapped[str] = mapped_column(String(), nullable=True)
+    weblink: Mapped[str] = mapped_column(String(), nullable=True)
+
+    tags: Mapped[List[ProjectTag]] = relationship(
+        secondary=association_projects_tags, back_populates="projects"
+    )
 
     def __repr__(self) -> str:
         return f"Project: ID: {self.id}; name: {self.name}"
+
+class BlogTag(Base):
+    __tablename__ = "blog_tags"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+
+    blogs: Mapped[List["Blog"]] = relationship(
+        secondary=association_blogs_tags, back_populates="tags"
+    )
 
 class Blog(Base):
     __tablename__ = "blogs"
@@ -26,3 +68,7 @@ class Blog(Base):
     name: Mapped[str] = mapped_column(String())
     content: Mapped[str] = mapped_column(String())
     date: Mapped[datetime.date] = mapped_column(Date())
+
+    tags: Mapped[List[BlogTag]] = relationship(
+        secondary=association_blogs_tags, back_populates="blogs"
+    )
